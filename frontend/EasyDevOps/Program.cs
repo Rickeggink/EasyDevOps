9418318d678fc60603a69de7ac2ad69ff3b2fd56
Rickeggink <552672@student.saxion.nl>
2024-11-29 17:02:01 +0100

    code

---
+++ frontend/EasyDevOps/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace EasyDevOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom bij EasyDevOps!");
+        }
+    }
+}
